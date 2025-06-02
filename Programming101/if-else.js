bffb4db720aae59c94efdfa43405a60ecceadfc2
Syed <73422322+syedbarkath980@users.noreply.github.com>
2025-06-02 23:38:58 +0530

    add if-else file

---
+++ Programming101/if-else.js
@@ -0,0 +1,9 @@
+/* If-Else is used to perform different actions based on conditions. */
+
+let age = 20;
+
+if (age >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a minor.");
+}
