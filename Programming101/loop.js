662c9e57309ae8ce1c75a3098db5e82f9bfa9d81
Syed <73422322+syedbarkath980@users.noreply.github.com>
2025-06-02 23:39:52 +0530

    add loop file

---
+++ Programming101/loop.js
@@ -0,0 +1,13 @@
+/* While loop repeats the block as long as the condition is true. */
+
+let count = 1;
+while (count <= 5) {
+  console.log("Count is: " + count);
+  count++;
+}
+
+/* For loop is often used when the number of iterations is known. */
+
+for (let i = 1; i <= 5; i++) {
+  console.log("Iteration: " + i);
+}
